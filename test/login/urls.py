c22ae7c5c65e367c2f761f086cb80912db7c25ac
theundergroundt <139340193+theundergroundt@users.noreply.github.com>
2023-10-08 21:17:41 +0900

    로그인 및 회원가입 구현
    pip install djangorestframework
    pip install djangorestframework-simplejwt
    
    이거 두개 설치 하세용

--- test/login/urls.py
+++ test/login/urls.py
@@ -1,9 +1,21 @@
-from django.urls import path,include
-from .views import SignUpView, SignInView
-from django.contrib import admin
+# from django.urls import path,include
+# from .views import SignUpView, SignInView
+# from django.contrib import admin
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('/up', SignUpView.as_view()), 
+#     path('/in', SignInView.as_view()),
+# ] 
+
+from django.urls import include, path
+from . import views
+
+login_patterns = [
+    path('normal/', views.login, name='login'),
+]
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('/up', SignUpView.as_view()), 
-    path('/in', SignInView.as_view()),
-] 
+    path('signup/', views.signup, name='signup'),
+    path('login/', include(login_patterns)),
+]
