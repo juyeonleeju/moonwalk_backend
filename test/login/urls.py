2780b6b0f28703a85a39fad02603e7755ae2bbf8
juyeon320 <ju010320@naver.com>
2023-10-09 19:15:28 +0900

    Update urls.py

--- test/login/urls.py
+++ test/login/urls.py
@@ -1,17 +1,11 @@
 from django.urls import include, path
 from . import views
-from django.urls import path
-from . import views
 
 login_patterns = [
     path('normal/', views.login, name='login'),
 ]
 
-urlpatterns = [
-    # 객체 나열하고 새로운 객체 만들기
-    path('your-models/', views.YourModelAPIView.as_view(), name='your-model-list'),
-
-    # /your-models/1/과 같은 URL에 YourModelDetailAPIView를 매핑합니다. 여기서 1은 객체의 주요 키입니다.
-    # 이 패턴은 개별 객체의 세부 정보, 업데이트 및 삭제를 처리
-    path('your-models/<int:pk>/', views.YourModelDetailAPIView.as_view(), name='your-model-detail'),
+urlpatterns = [ 
+    path('signup/', views.signup, name='signup'),
+    path('login/', include(login_patterns)),
 ]
